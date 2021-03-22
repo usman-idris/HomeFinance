44b9166ca811f53406211dcb0f8b6052754bb74b
Usman Idris Kwaru <kwaru91@gmail.com>
2021-03-22 22:23:28 +0100

    Add Panel, Textfields and labels

--- HomeFinance/src/gui/Login.java
+++ HomeFinance/src/gui/Login.java
@@ -7,13 +7,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
- * @author USER
+ * @author Usman Idris Kwaru
  *
  */
 public class Login extends JFrame implements ActionListener {
-	
+	JPanel loginPanel;
+	JTextField txtUserName, txtPassword;
+	JLabel lblUserName, lblPassword;
 	
 
 	@Override
